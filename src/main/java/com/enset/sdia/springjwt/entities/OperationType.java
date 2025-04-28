f21ecda356a1e1a507f05882b447ad62f2ad22e2
walidmoufadil <walid.moufadil-etu@etu.univh2c.ma>
2025-04-28 13:56:53 +0000

    add entity package

---
+++ src/main/java/com/enset/sdia/springjwt/entities/OperationType.java
@@ -0,0 +1,5 @@
+package com.enset.sdia.springjwt.entities;
+
+public enum OperationType {
+    DEBIT, CREDIT
+}
---
+++ src/main/java/com/enset/sdia/springjwt/entities/SavingAccount.java
@@ -0,0 +1,16 @@
+package com.enset.sdia.springjwt.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("SA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavingAccount extends BankAccount {
+    private double interestRate;
+}
